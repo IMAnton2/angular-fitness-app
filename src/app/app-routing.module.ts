4796f7c94580167947820940b27f130dd85e4a03
IMAnton2 <imanton2@gmail.com>
2021-04-08 23:17:03 +0300

    1 file modified const routes: Routes = [{ path: "", component: WelcomeComponent }];

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import welcomeComponentCss from "./welcome/welcome.component.css";
+import { WelcomeComponent } from "./welcome/welcome.component";
 
-const routes: Routes = [{ path: "", component: welcomeComponentCss }];
+const routes: Routes = [{ path: "", component: WelcomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
