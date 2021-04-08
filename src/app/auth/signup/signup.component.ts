c5360dffb0cef4afc558c9e2ae2fbf89927ba2ec
IMAnton2 <imanton2@gmail.com>
2021-04-08 22:51:06 +0300

    1 file modified, 6 added training + current

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-signup",
-  templateUrl: "signup.component.html",
-  styleUrls: ["signup.component.css"]
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent implements OnInit {
   constructor() {}
