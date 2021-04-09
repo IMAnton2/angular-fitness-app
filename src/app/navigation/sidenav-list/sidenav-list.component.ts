28d35485cc689eaab73108a9d894f7d0bdd20f1f
IMAnton2 <imanton2@gmail.com>
2021-04-09 19:27:35 +0300

    6 files added added sideNav and header components

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "sidenav-list-app",
+  templateUrl: "./sidenav-list.component.html",
+  styleUrls: ["./sidenav-list.component.css"]
+})
+export class sidenavListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
